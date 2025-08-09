58f0e6186ef271d6ddfa0ac3371db1a302e2e7b6
angel-jeyriel <angeljeyriel@gmail.com>
2025-08-10 00:55:42 +0100

    Model - migration commit

---
+++ app/Models/Transaction.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    // protected $fillable = ['user_id', 'description', 'amount', 'category_id', 'transaction_date'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
