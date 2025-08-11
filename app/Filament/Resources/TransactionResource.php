4c0b443da28955a90f7d1eab80e66306b5b3c0a9
angel-jeyriel <angeljeyriel@gmail.com>
2025-08-11 02:35:32 +0100

    Next commit

--- app/Filament/Resources/TransactionResource.php
+++ app/Filament/Resources/TransactionResource.php
@@ -4,14 +4,18 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\TransactionResource\Pages;
 use App\Filament\Resources\TransactionResource\RelationManagers;
+use App\Models\Category;
 use App\Models\Transaction;
 use Filament\Forms;
 use Filament\Forms\Form;
+use Filament\Infolists\Components\Tabs\Tab;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
+use Illuminate\Container\Attributes\Auth;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Foundation\Http\FormRequest;
 
 class TransactionResource extends Resource
 {
@@ -23,15 +27,32 @@ class TransactionResource extends Resource
     {
         return $form
             ->schema([
-                //
-            ]);
+                Forms\Components\Hidden::make('user_id')->default(auth()->id()),
+                Forms\Components\TextInput::make('description')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('amount')->numeric()->required(),
+                Forms\Components\Select::make('category_id')
+                    ->label('Category')
+                    ->options(function () {
+                        $userId = auth()->id();
+                        return Category::whereNull('user_id') // defaults
+                            ->orWhere('user_id', $userId)   // user's categories
+                            ->pluck('name', 'id');
+                    })
+                    ->searchable()
+                    ->required(),
+                Forms\Components\DatePicker::make('transaction_date')->required()
+            ])->columns(2);
     }
 
     public static function table(Table $table): Table
     {
         return $table
             ->columns([
-                //
+                Tables\Columns\TextColumn::make('description')
+                    ->searchable()
+                    ->sortable(),
             ])
             ->filters([
                 //
---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $defaults = ['Food', 'Transport', 'Utility', 'Entertainment'];
+
+        foreach ($defaults as $name) {
+            Category::firstOrCreate([
+                'name' => $name,
+                'user_id' => null,
+            ]);
+        }
+    }
+}
